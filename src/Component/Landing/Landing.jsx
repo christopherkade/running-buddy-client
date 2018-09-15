3761bf3d3598e335ab3b324cb2e681c2c7c5659a
Christopher Kade <c.kade96@gmail.com>
2018-09-15 11:00:19 +0200

    Updated landing page Updated elint config file

--- src/Component/Landing/Landing.jsx
+++ src/Component/Landing/Landing.jsx
@@ -1,48 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Landing.css';
-import runner from './paris-runner.jpg';
-import runner2 from './paris-runner2.jpg';
+import scrollToComponent from 'react-scroll-to-component';
+import arrow from './arrow.svg';
 
-function Landing() {
-  return (
-    <div className="landing">
-      <h1 className="landing-title">Runnig Buddy, your fitness companion</h1>
+class Landing extends Component {
+  constructor() {
+    super();
+    this.block1Ref = React.createRef();
+  }
 
-      <div className="container first-row">
-        <div className="row">
-          <div className="col-sm">
-            <img className="landing-img" src={runner} alt="Runner" />
+  scrollToElement = () => {
+    scrollToComponent(this.block1, {
+      offset: 1000,
+      align: 'top',
+      duration: 1500
+    });
+  };
+
+  render() {
+    return (
+      <div className="landing-wrapper">
+        <div className="landing">
+          <h1 className="landing-title">Runnig Buddy, your fitness companion</h1>
+          <button
+            type="button"
+            className="btn btn-primary landing-button"
+            onClick={this.scrollToElement}
+          >
+            Discover us
+          </button>
+        </div>
+
+        <div
+          className="landing-block1"
+          ref={div => {
+            this.block1 = div;
+          }}
+        >
+          <img className="arrow" src={arrow} alt="arrow" />
+          <div className="landing-text-block block-right">
+            <h3 className="landing-subtext">As easy as ready, set, go !</h3>
+
+            <p>Just create or join a session in your neighborhood and you're ready to go !</p>
           </div>
-          <div className="col-sm landing-text landing-text-right">
-            <h3 className="landing-subtitle">As easy as ready, set, go !</h3>
-            <p className="landing-description">
-              Just create a running session on our platform and find running partners in a matter of
-              minutes.
-            </p>
+          <img className="arrow arrow-invert" src={arrow} alt="arrow" />
+          <div className="landing-text-block block-left">
+            <h3 className="landing-subtext">Find new buddies, everyday</h3>
+
+            <p>Join a massive network of like-minded people !</p>
           </div>
         </div>
       </div>
-
-      <div className="container second-row">
-        <div className="row">
-          <div className="col-sm landing-text landing-text-right">
-            <h3 className="landing-subtitle">New friends, everyday</h3>
-            <p className="landing-description">
-              Our large network of runners allows you to find like-minded people on a daily basis.
-            </p>
-          </div>
-          <div className="col-sm">
-            <img className="landing-img landing-img-rect" src={runner2} alt="Runner" />
-          </div>
-        </div>
-      </div>
-
-      <h2 className="landing-cta">What are you waiting for? Sign up now !</h2>
-      <button type="button" className="btn btn-secondary">
-        Secondary
-      </button>
-    </div>
-  );
+    );
+  }
 }
 
 export default Landing;
