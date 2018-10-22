3824296afbd596f32ebae04eb1ae28fac44fa75c
Christopher Kade <c.kade96@gmail.com>
2018-10-22 10:25:55 +0200

    Added tests for SignIn, SignUp and Navigation

--- src/Component/Sign/In/SignIn.test.jsx
+++ src/Component/Sign/In/SignIn.test.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import * as enzyme from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import SignIn from './SignIn';
 
 let wrapper = null;
 
-enzyme.configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
 
 beforeEach(() => {
-  wrapper = enzyme.shallow(<SignIn />);
+  wrapper = shallow(<SignIn />);
 });
 
 it('renders without crashing', () => {
@@ -22,3 +22,20 @@ it('should have valid default email', () => {
 it('should have valid default password', () => {
   expect(wrapper.instance().state.passwordValue).toBe('');
 });
+
+it('should have the correct home navigation links', () => {
+  expect(wrapper.find('.logo-link').props().to).toBe('/');
+});
+
+it('should have the correct sign-up navigation links', () => {
+  expect(wrapper.find('.sign-up-link').props().to).toBe('/sign-up');
+});
+
+/* it('should handle input change', () => {
+  const event = { target: { email: 'c.kade96@gmail.com', password: 'abcdefg' } };
+  const mountedWrapper = mount(<SignIn />);
+  const updateEmailSpy = sinon.spy(mountedWrapper.instance(), 'updateEmailValue');
+  mountedWrapper.update();
+  mountedWrapper.ref('email').simultate('change', event);
+  expect(updateEmailSpy.calledOnce).to.equal(true);
+}); */
--- src/Component/Sign/Up/SignUp.test.jsx
+++ src/Component/Sign/Up/SignUp.test.jsx
@@ -26,3 +26,11 @@ it('should have valid default password', () => {
 it('should have a valud default username', () => {
   expect(wrapper.instance().state.usernameValue).toBe('');
 });
+
+it('should have the correct home navigation links', () => {
+  expect(wrapper.find('.logo-link').props().to).toBe('/');
+});
+
+it('should have the correct sign-in navigation links', () => {
+  expect(wrapper.find('.sign-in-link').props().to).toBe('/sign-in');
+});
