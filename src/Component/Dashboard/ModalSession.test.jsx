7904fbeb846f59302ec56774e54134927d68ccfc
Christopher Kade <c.kade96@gmail.com>
2018-10-27 22:38:14 +0200

    Added ModalSession unit testing

---
+++ src/Component/Dashboard/ModalSession.test.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import * as enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import ModalSession from './ModalSession';
+
+let wrapper = null;
+
+enzyme.configure({ adapter: new Adapter() });
+
+beforeEach(() => {
+  const session = {
+    title: 'Session title',
+    description: 'Session description'
+  };
+  const modal = true;
+  const toggleModalFunc = () => {};
+  wrapper = enzyme.shallow(
+    <ModalSession session={session} modal={modal} toggleModal={toggleModalFunc} />
+  );
+});
+
+it('renders without crashing', () => {
+  expect(wrapper).not.toBe(null);
+});
+
+it('should have member badges (>= 0)', () => {
+  expect(wrapper.find('.badgeModal').length).toBeGreaterThanOrEqual(0);
+});
+
+it('should have a date', () => {
+  expect(wrapper.find('.modalDate>Input').props().value).not.toBe('');
+});
