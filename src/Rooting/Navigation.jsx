52b6098a455896043f5478fedfff2cf21edaf8e4
Christopher Kade <c.kade96@gmail.com>
2018-11-04 21:31:15 +0100

    Fixed navbar navigation bug

--- src/Rooting/Navigation.jsx
+++ src/Rooting/Navigation.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar, NavbarBrand, Nav, UncontrolledDropdown, DropdownToggle } from 'reactstrap';
+import { Link } from 'react-router-dom';
 import { FaUser } from 'react-icons/fa';
 import './Navigation.css';
 import Setting from '../Component/Setting';
@@ -8,9 +9,9 @@ const Navigation = props => (
   <div>
     <Navbar className="navbar fixed-top" light>
       <NavbarBrand>
-        <a href="/home" className="title">
+        <Link to="/home" className="title">
           Running Buddy
-        </a>
+        </Link>
       </NavbarBrand>
       <Nav className="ml-auto" navbar>
         <UncontrolledDropdown>
